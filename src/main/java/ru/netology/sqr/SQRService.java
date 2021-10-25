e0d73f52894b8a0e33207f7828b627e777db366d
Nikolai Pustovoit <nikolaikop@gmail.com>
2021-10-24 20:45:12 -0600

    updated test

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,12 +2,20 @@ package ru.netology.sqr;
 
 public class SQRService {
     public int calculate(int bottomBorder, int upperBorder) {
-        int j = 0;
+        int sqrNum = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= bottomBorder && i * i <= upperBorder) {
-                j = j + 1;
+            int doubleI = i * i;
+            if (doubleI >= bottomBorder && doubleI <= upperBorder) {
+                sqrNum = sqrNum + 1;
             }
         }
-        return j;
+        return sqrNum;
     }
+
+/*    public static void main(String[] args) {
+        SQRService service = new SQRService();
+        System.out.println(service.calculate(200,300));
+        System.out.println(service.calculate(0,100));
+        System.out.println(service.calculate(300,320));
+    }*/
 }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,18 +1,32 @@
 package ru.netology.sqr;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
 
-    @ParameterizedTest
-    @CsvSource(value = {"200,300", "300,400", "400,500"})
-    void calculateRegular(int bottomBorder, int upperBorder) {
+    @org.junit.jupiter.api.Test
+    void calculate3SQRs() {
         SQRService service = new SQRService();
         int expected = 3;
-        int actual = service.calculate(bottomBorder, upperBorder);
+        int actual = service.calculate(200, 300);
+        assertEquals(expected, actual);
+        System.out.println("expected = " + expected + " and" + " actual = " + actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculate1SQR() {
+        SQRService service = new SQRService();
+        int expected = 1;
+        int actual = service.calculate(0, 100);
+        assertEquals(expected, actual);
+        System.out.println("expected = " + expected + " and" + " actual = " + actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculate0SQR() {
+        SQRService service = new SQRService();
+        int expected = 0;
+        int actual = service.calculate(300, 320);
         assertEquals(expected, actual);
         System.out.println("expected = " + expected + " and" + " actual = " + actual);
     }
